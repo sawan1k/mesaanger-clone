624b56d05f154da6c7bd58aa7ef8be091195168c
sawan1k <sawankushwaha222@gmail.com>
2024-01-04 20:46:49 +0530

    final error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,11 @@
 import bcrypt from "bcrypt"
-import NextAuth, { AuthOptions } from "next-auth";
-
+import NextAuth, { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import GithubProvider from 'next-auth/providers/github'
+import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
-
 import prisma from "@/app/libs/prismadb"
-
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
@@ -61,6 +58,7 @@ export const authOptions: AuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
 }
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
