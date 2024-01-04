a1d3c60ca5e06c3e76318e5353a8dd3638ecd0a9
sawan1k <sawankushwaha222@gmail.com>
2024-01-04 20:44:36 +0530

    nexterrror

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt"
-import  { AuthOptions } from "next-auth"
+import NextAuth, { AuthOptions } from "next-auth";
 
 import CredentialsProvider from "next-auth/providers/credentials"
 import GithubProvider from 'next-auth/providers/github'
@@ -8,7 +8,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 
 import prisma from "@/app/libs/prismadb"
-import NextAuth from "next-auth/next"
+
 
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
